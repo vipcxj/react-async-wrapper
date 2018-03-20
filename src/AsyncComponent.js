996d673aa8d94288957a7c331db5d4749a71b87e
vipcxj <vipcxj@126.com>
2018-03-20 13:59:20 +0800

    增加进度支持，已经相关story

--- src/AsyncComponent.js
+++ src/AsyncComponent.js
@@ -16,14 +16,19 @@ class AsyncComponent extends Component {
             ...preState.resolvedProps,
             ...resolvedProps,
         },
+        progress: {
+            ...preState.progress,
+            ...fromPairs(Object.keys(resolvedProps).map(k => [k, 1])),
+        }
     });
-    constructor(...args) {
-        super(...args);
+    constructor(props, ...args) {
+        super(props, ...args);
         this.errorComponent = null;
         this.state = {
             error: null,
             loading: true,
             resolvedProps: {},
+            progress: fromPairs(Object.keys(props.asyncProps || {}).map(k => [k, 0])),
         };
     }
 
@@ -35,6 +40,13 @@ class AsyncComponent extends Component {
     componentWillUnmount() {
         this.mounted = false;
     }
+    updateProgress = propName => progress => this.setState({
+        progress: {
+            ...this.state.progress,
+            [propName]: progress,
+        },
+    });
+
     load() {
         let pairs = toPairs(this.props.asyncProps || {})
             .filter(entry => entry[1]);
@@ -46,7 +58,7 @@ class AsyncComponent extends Component {
             return;
         }
         const { batch } = this.props;
-        pairs = pairs.map(([k, v]) => ([k, v()]));
+        pairs = pairs.map(([k, v]) => ([k, v(this.updateProgress(k))]));
         if (some(pairs, pair => isPromise(pair[1]))) {
             const promises = pairs.map(([k, p]) => {
                 if (isPromise(p)) {
@@ -54,10 +66,11 @@ class AsyncComponent extends Component {
                         if (!batch) {
                             if (this.mounted) {
                                 this.setState(AsyncComponent.updateResolvedProps({
-                                    k: res,
+                                    [k]: res,
                                 }));
                             } else {
                                 this.state.resolvedProps[k] = res;
+                                this.state.progress[k] = 1;
                             }
                         }
                         return [k, res];
@@ -69,10 +82,12 @@ class AsyncComponent extends Component {
                 if (this.mounted) {
                     this.setState({
                         resolvedProps: fromPairs(pairs),
+                        progress: fromPairs(pairs.map(([k]) => [k, 1])),
                         error: null,
                     });
                 } else {
                     this.state.resolvedProps = fromPairs(pairs);
+                    this.state.progress = fromPairs(pairs.map(([k, v]) => [k, 1]));
                     this.state.error = null;
                 }
             }).catch((e) => {
@@ -102,16 +117,16 @@ class AsyncComponent extends Component {
             component: Comp,
             children, // eslint-disable-line react/prop-types
         } = this.props;
-        const { error, loading, resolvedProps } = this.state;
+        const { error, loading, resolvedProps, progress } = this.state;
         const { children: ignored, ...restResolvedProps } = resolvedProps;
         if (loading) {
-            return <LoadingComponent {...this.props} {...restResolvedProps} />;
+            return <LoadingComponent {...this.props} {...restResolvedProps} progress={progress} />;
         }
         if (error) {
-            return <ErrorComponent {...this.props} {...restResolvedProps} error={error} />;
+            return <ErrorComponent {...this.props} {...restResolvedProps} progress={progress} error={error} />;
         }
         if (Comp) {
-            return <Comp {...this.props} {...resolvedProps} />;
+            return <Comp {...this.props} {...resolvedProps} progress={progress} />;
         }
         // noinspection JSUnresolvedFunction JSCheckFunctionSignatures
         const newChildren = React.Children
@@ -134,6 +149,7 @@ const {
     element,
 } = PropTypes;
 
+// noinspection JSUnresolvedVariable
 AsyncComponent.propTypes = {
     batch: bool,
     // eslint-disable-next-line react/forbid-prop-types
--- stories/index.js
+++ stories/index.js
@@ -8,8 +8,8 @@ const Demo = ({a, b, c, d}) => (
     <ul>
         <li>{`a: ${a}`}</li>
         <li>{`b: ${b}`}</li>
-        <li>{`b: ${c}`}</li>
-        <li>{`b: ${d}`}</li>
+        <li>{`c: ${c}`}</li>
+        <li>{`d: ${d}`}</li>
     </ul>
 );
 
@@ -18,14 +18,61 @@ const lazyReturn = (v, t) => async () => {
     return v;
 };
 
+const progressReturn = (v, t) => async (updater) => {
+    const step = t / 100;
+    for (let i = 0; i < 100; ++i) {
+        await sleep(step);
+        updater(i / 100);
+    }
+    return v;
+};
+
+const SimpleLoading = () => "Loading ...";
+const DetailLoading = ({ a, b, c, d, progress }) => (
+  <ul>
+      <li>{`a: ${progress.a === 1 ? a : 'Loading ...'}`}</li>
+      <li>{`b: ${progress.b === 1 ? b : 'Loading ...'}`}</li>
+      <li>{`c: ${progress.c === 1 ? c : 'Loading ...'}`}</li>
+      <li>{`d: ${progress.d === 1 ? d : 'Loading ...'}`}</li>
+  </ul>
+);
+const ProgressLoading = ({ a, b, c, d, progress }) => (
+    <ul>
+        <li>{`a: ${(progress.a * 100).toFixed()}%`}</li>
+        <li>{`b: ${(progress.b * 100).toFixed()}%`}</li>
+        <li>{`c: ${(progress.c * 100).toFixed()}%`}</li>
+        <li>{`d: ${(progress.d * 100).toFixed()}%`}</li>
+    </ul>
+);
+
 storiesOf('async component', module)
 .add('without wrapped', () => <Demo a={1} b={2} c={3} d={4}/>)
 .add('with basic wrapped', () => (
-  <AsyncComponent asyncProps={{
+  <AsyncComponent loadingComponent={SimpleLoading} asyncProps={{
       a: lazyReturn(1, 1000),
       b: lazyReturn(2, 1000),
       c: lazyReturn(3, 1000),
       d: lazyReturn(4, 1000),
+  }}>
+      <Demo/>
+  </AsyncComponent>
+))
+.add('with detail loading wrapped', () => (
+  <AsyncComponent loadingComponent={DetailLoading} batch={false} asyncProps={{
+      a: lazyReturn(1, 4000),
+      b: lazyReturn(2, 3000),
+      c: lazyReturn(3, 2000),
+      d: lazyReturn(4, 1000),
+  }}>
+      <Demo/>
+  </AsyncComponent>
+))
+.add('with progress loading wrapped', () => (
+  <AsyncComponent loadingComponent={ProgressLoading} batch={false} asyncProps={{
+      a: progressReturn(1, 4000),
+      b: progressReturn(2, 3000),
+      c: progressReturn(3, 2000),
+      d: progressReturn(4, 1000),
   }}>
       <Demo/>
   </AsyncComponent>
