beda9b39729ae163ef24e50c3156a03178d357ef
vipcxj <vipcxj@126.com>
2018-03-27 19:47:20 +0800

    add asyncComponent prop and update readme.

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Using [npm](https://www.npmjs.com/):
 
     $ npm install --save react-async-wrapper
     
-```jsx harmony
+```javascript
 import { AsyncComponent } from 'react-async-wrapper';
 
 //js version sleep.
@@ -64,104 +64,57 @@ export const AsyncDemo = () => {
 ## API
 
 ### Properties
-<table class="table table-bordered table-striped">
-  <thead>
-    <tr>
-      <th style="width: 100px;">Name</th>
-      <th style="width: 50px;">Type</th>
-      <th>Default</th>
-      <th>Description</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>batch</td>
-      <td>bool</td>
-      <td>false</td>
-      <td>
-        If true, only when all async props are resolved, the wrapped component is rendered again. 
-        Otherwise, the wrapped component will be updated when any async prop is resolved.
-      </td>
-    </tr>
-    <tr>
-      <td>asyncJobs</td>
-      <td>arrayOf(func)</td>
-      <td>[]</td>
-      <td>
-        The async jobs. 
-        Compared to async props, they will not provide the props to the wrapped component, 
-        but they should be done before the wrapped component finally render.
-        The word 'finally' means the wrapped component will be updated sevial time if 'batch' set to false.
-      </td>
-    </tr>
-    <tr>
-      <td>asyncProps</td>
-      <td>objectOf(func)</td>
-      <td>{}</td>
-      <td>
-        All async props should be declared here.They should be functions. 
-        If the function return a promise, it will be a real async prop, otherwise it is still a sync prop.
-        The es6 async function is supported. In fact it is just a function return promise.
-      </td>
-    </tr>
-    <tr>
-      <td>asyncPropOpts</td>
-      <td>objectOf(shape({ defaultProp: any }))</td>
-      <td>{}</td>
-      <td>
-        The option of the async props. At this moment, only 'defaultProp' is supported.
-      </td>
-    </tr>
-    <tr>
-      <td>asyncPropsMapper</td>
-      <td>func(props):props</td>
-      <td>props => props</td>
-      <td>
-        Accept the resolved async props, and return the props provide to the wrapped component.
-      </td>
-    </tr>
-    <tr>
-      <td>component</td>
-      <td>react component</td>
-      <td>null</td>
-      <td>
-        If specialized, the async wrapper will use this as the wrapped component instead of the children components.
-      </td>
-    </tr>
-    <tr>
-      <td>errorComponent</td>
-      <td>react component</td>
-      <td>() => null</td>
-      <td>
-        This component will be used to show the error.
-      </td>
-    </tr>
-    <tr>
-      <td>loadingComponent</td>
-      <td>react component</td>
-      <td>() => null</td>
-      <td>
-        When the async jobs and async props have not been resolved yet, this component will be rendered. 
-        However, if this prop is not specialized, the wrapped component with default and partial resolved props will be rendered instead.
-      </td>
-    </tr>
-    <tr>
-      <td>onError</td>
-      <td>func(error)</td>
-      <td>(error) => null</td>
-      <td>
-        The error callback. 
-        It will be called when a error is throwed 
-        during the async jobs is running or the async props is resolved.
-      </td>
-    </tr>
-    <tr>
-      <td>delay</td>
-      <td>number</td>
-      <td>0</td>
-      <td>
-        A number greater than 0 will force the wrapped component rendering with a delay.
-      </td>
-    </tr>
-  </tbody>
-</table>
+- *batch* - **bool** `false`
+
+  If true, only when all async props are resolved, the wrapped component is rendered again. 
+  Otherwise, the wrapped component will be updated when any async prop is resolved.
+
+- *asyncJobs* - **[ func() : (Promise\<any\> | any) ]** `[]`
+
+  The async jobs. 
+  Compared to async props, they will not provide the props to the wrapped component, 
+  but they should be done before the wrapped component finally render.
+  The word 'finally' means the wrapped component will be updated sevial time if 'batch' set to false.
+  
+- *asyncProps* - **{ property: func( progressUpdater: ( func(number):void ) ) : (Promise\<any> | any) }** `{}`
+
+  All async props should be declared here.They should be functions. 
+  If the function return a promise, it will be a real async prop, otherwise it is still a sync prop.
+  The es6 async function is supported. In fact it is just a function return promise.
+  The async component will provide every function with a method to update the progress which is a number range from 0 to 1.
+  
+- *asyncPropOpts* - **{ property: { defaultProp: any } }** `{}`
+
+  The option of the async props. At this moment, only 'defaultProp' is supported.
+  
+- *asyncPropsMapper* - **func( props: { property: any } ):{ property: any }** `props => props`
+
+  Accept the resolved async props, and return the props provide to the wrapped component.
+  
+- *component* - **Component** `null`
+
+  If specialized, the async wrapper will use this as the wrapped component instead of the children components.
+  
+- *asyncComponent* - **func() : ( Promise\<Component\> | Component )** `null`
+
+  If specialized, the async wrapper will use this as the wrapped component when resolved instead of component prop and the children components.
+  
+- *errorComponent* - **Component** `() => null`
+
+  This component will be used to show the error.
+  
+- *loadingComponent* - **Component** `() => null`
+
+  When the async jobs and async props have not been resolved yet, this component will be rendered. 
+  However, if this prop is not specialized, the wrapped component with default and partial resolved props will be rendered instead.
+  
+- *onError* - **func( error: any ) : void** `() => null`
+
+  The error callback. 
+  It will be called when a error is throwed 
+  during the async jobs is running or the async props is resolved.
+  
+- *delay* - **number** : `0`
+
+  A number greater than 0 will force the wrapped component rendering with a delay.
+  
--- src/AsyncComponent.js
+++ src/AsyncComponent.js
@@ -6,179 +6,210 @@ import { some, toPairs, fromPairs, isFunction, mapValues } from './utils';
 
 const DefaultLoadingComponent = () => null;
 const DefaultErrorComponent = () => null;
+const SymbolComp = Symbol('component');
 const SymbolJob = Symbol('job');
 
 const majorVersion = React.version ? Number.parseInt(React.version.slice(0, React.version.indexOf('.')), 10) : 0;
 
 class AsyncComponent extends Component {
-    static updateResolvedProps = resolvedProps => preState => ({
-      resolvedProps: {
-        ...preState.resolvedProps,
-        ...resolvedProps,
-      },
-      progress: {
-        ...preState.progress,
-        ...fromPairs(Object.keys(resolvedProps).map(k => [k, 1])),
-      },
-    });
-    constructor(props, ...args) {
-      super(props, ...args);
-      this.errorComponent = null;
-      this.state = {
-        error: null,
-        loading: true,
-        resolvedProps: {},
-        progress: fromPairs(Object.keys(props.asyncProps || {}).map(k => [k, 0])),
-      };
+  static updateResolvedProps = resolvedProps => preState => ({
+    resolvedProps: {
+      ...preState.resolvedProps,
+      ...resolvedProps,
+    },
+    progress: {
+      ...preState.progress,
+      ...fromPairs(Object.keys(resolvedProps).map(k => [k, 1])),
+    },
+  });
+  constructor(props, ...args) {
+    super(props, ...args);
+    this.errorComponent = null;
+    this.state = {
+      error: null,
+      loading: true,
+      resolvedProps: {},
+      component: null,
+      progress: fromPairs(Object.keys(props.asyncProps || {}).map(k => [k, 0])),
+    };
+  }
+  setStateSafe = (newState) => {
+    if (this.mounted) {
+      this.setState(newState);
+    } else {
+      Object.assign(this.state, newState);
     }
+  };
+  componentDidMount() {
+    this.mounted = true;
+    this.load();
+  }
 
-    componentDidMount() {
-      this.mounted = true;
-      this.load();
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+  updateProgress = propName => progress => this.setState({
+    progress: {
+      ...this.state.progress,
+      [propName]: progress,
+    },
+  });
+  loadSuccess = (pairs) => {
+    let compPair = null;
+    for (let i = 0; i < pairs.length; ++i) {
+      const [k] = pairs[i];
+      if (k === SymbolComp) {
+        [compPair] = pairs.splice(i, 1);
+      }
     }
-
-    componentWillUnmount() {
-      this.mounted = false;
+    const newState = {
+      resolvedProps: fromPairs(pairs),
+      progress: fromPairs(pairs.map(([k]) => [k, 1])),
+      error: null,
+      loading: false,
+    };
+    if (compPair) {
+      newState.component = compPair[1];
     }
-    updateProgress = propName => progress => this.setState({
-      progress: {
-        ...this.state.progress,
-        [propName]: progress,
-      },
-    });
-    loadSuccess = (pairs) => {
-      if (this.mounted) {
+    this.setStateSafe(newState);
+  };
+  load() {
+    const {
+      asyncComponent, asyncJobs, asyncProps, onError, delay,
+    } = this.props;
+    try {
+      let pairs = [
+        ...(asyncComponent ? [[SymbolComp, asyncComponent]] : []),
+        ...(asyncJobs || []).map(job => [SymbolJob, job]),
+        ...toPairs(asyncProps || {}),
+      ].filter(entry => entry[1]);
+      if (some(pairs, entry => !isFunction(entry[1]))) {
+        const error = new Error('The async job or async prop must be a function.');
         this.setState({
-          resolvedProps: fromPairs(pairs),
-          progress: fromPairs(pairs.map(([k]) => [k, 1])),
-          error: null,
+          error,
           loading: false,
         });
-      } else {
-        this.state.resolvedProps = fromPairs(pairs);
-        this.state.progress = fromPairs(pairs.map(([k]) => [k, 1]));
-        this.state.error = null;
-        this.state.loading = false;
-      }
-    };
-    load() {
-      const {
-        asyncJobs, asyncProps, onError, delay,
-      } = this.props;
-      try {
-        let pairs = [
-          ...(asyncJobs || []).map(job => [SymbolJob, job]),
-          ...toPairs(asyncProps || {}),
-        ].filter(entry => entry[1]);
-        if (some(pairs, entry => !isFunction(entry[1]))) {
-          const error = new Error('The async job or async prop must be a function.');
-          this.setState({
-            error,
-            loading: false,
-          });
-          if (onError) {
-            onError(error);
-          }
-          return;
-        }
-        const { batch } = this.props;
-        pairs = pairs.map(([k, v]) => ([k, v(this.updateProgress(k))]));
-        if (delay > 0) {
-          pairs.push([SymbolJob, new Promise(resolve => setTimeout(resolve, delay))]);
-        }
-        if (some(pairs, pair => isPromise(pair[1]))) {
-          const promises = pairs.map(([k, p]) => {
-            if (isPromise(p)) {
-              return p.then((res) => {
-                if (!batch && typeof k !== 'symbol') {
-                  if (this.mounted) {
-                    this.setState(AsyncComponent.updateResolvedProps({
-                      [k]: res,
-                    }));
-                  } else {
-                    this.state.resolvedProps[k] = res;
-                    this.state.progress[k] = 1;
-                  }
-                }
-                return [k, res];
-              });
-            }
-            if (!batch && typeof k !== 'symbol') {
-              this.setState(AsyncComponent.updateResolvedProps({
-                [k]: p,
-              }));
-            }
-            return [k, p];
-          });
-          Promise.all(promises).then((pairs) => {
-            this.loadSuccess(pairs.filter(([k]) => typeof k !== 'symbol'));
-          }).catch((e) => {
-            if (this.mounted) {
-              this.setState({
-                error: e,
-              });
-            } else {
-              this.state.error = e;
-            }
-            if (onError) {
-              onError(e);
-            }
-          }).finally(() => {
-            if (this.mounted) {
-              this.setState({
-                loading: false,
-              });
-            } else {
-              this.state.loading = false;
-            }
-          });
-        } else {
-          this.loadSuccess(pairs.filter(([k]) => typeof k !== 'symbol'));
-        }
-      } catch (error) {
         if (onError) {
           onError(error);
         }
+        return;
+      }
+      const { batch } = this.props;
+      pairs = pairs.map(([k, v]) => ([k, v(typeof k !== 'symbol' ? this.updateProgress(k) : null)]));
+      if (delay > 0) {
+        pairs.push([SymbolJob, new Promise(resolve => setTimeout(resolve, delay))]);
+      }
+      if (some(pairs, pair => isPromise(pair[1]))) {
+        const promises = pairs.map(([k, p]) => {
+          if (isPromise(p)) {
+            return p.then((res) => {
+              if (!batch && typeof k !== 'symbol') {
+                if (this.mounted) {
+                  this.setState(AsyncComponent.updateResolvedProps({
+                    [k]: res,
+                  }));
+                } else {
+                  this.state.resolvedProps[k] = res;
+                  this.state.progress[k] = 1;
+                }
+              }
+              if (!batch && k === SymbolComp) {
+                this.setStateSafe({
+                  component: res,
+                });
+              }
+              return [k, res];
+            });
+          }
+          if (!batch && typeof k !== 'symbol') {
+            this.setState(AsyncComponent.updateResolvedProps({
+              [k]: p,
+            }));
+          }
+          if (!batch && k === SymbolComp) {
+            this.setState({
+              component: p,
+            });
+          }
+          return [k, p];
+        });
+        Promise.all(promises).then((pairs) => {
+          this.loadSuccess(pairs.filter(([k]) => typeof k !== 'symbol'));
+        }).catch((e) => {
+          if (this.mounted) {
+            this.setState({
+              error: e,
+            });
+          } else {
+            this.state.error = e;
+          }
+          if (onError) {
+            onError(e);
+          }
+        }).finally(() => {
+          if (this.mounted) {
+            this.setState({
+              loading: false,
+            });
+          } else {
+            this.state.loading = false;
+          }
+        });
+      } else {
+        this.loadSuccess(pairs.filter(([k]) => typeof k !== 'symbol'));
+      }
+    } catch (error) {
+      if (onError) {
+        onError(error);
       }
     }
+  }
 
-    render() {
-      const {
-        errorComponent: ErrorComponent,
-        loadingComponent: LoadingComponent,
-        asyncPropOpts,
-        asyncPropsMapper,
-        component: Comp,
-        children, // eslint-disable-line react/prop-types
-      } = this.props;
-      const {
-        error, loading, resolvedProps, progress,
-      } = this.state;
-      const wrappedProps = asyncPropsMapper(Object.assign(
-        {},
-        mapValues(asyncPropOpts, opt => opt.defaultProp, opt => opt && opt.defaultProp),
-        resolvedProps,
-      ));
-      const { children: ignored, ...restResolvedProps } = wrappedProps;
-      if (loading && LoadingComponent !== DefaultLoadingComponent) {
-        return <LoadingComponent {...this.props} {...restResolvedProps} progress={progress} />;
-      }
-      if (error) {
-        return <ErrorComponent {...this.props} {...restResolvedProps} progress={progress} error={error} />;
-      }
-      if (Comp) {
-        return <Comp {...wrappedProps} progress={progress} />;
-      }
-      // noinspection JSUnresolvedFunction JSCheckFunctionSignatures
-      const newChildren = React.Children
-        .map(children, child => React.cloneElement(child, restResolvedProps));
-      if (newChildren.length === 1) {
-        return newChildren[0];
-      } else if (newChildren.length > 1) {
-        return majorVersion >= 16 ? newChildren : <div> {newChildren} </div>;
-      }
-      return null;
+  render() {
+    const {
+      errorComponent: ErrorComponent,
+      loadingComponent: LoadingComponent,
+      asyncPropOpts,
+      asyncPropsMapper,
+      asyncComponent,
+      children, // eslint-disable-line react/prop-types
+    } = this.props;
+    let { component: Comp } = this.props;
+    const {
+      error, loading, resolvedProps, progress,
+    } = this.state;
+    const wrappedProps = asyncPropsMapper(Object.assign(
+      {},
+      mapValues(asyncPropOpts, opt => opt.defaultProp, opt => opt && opt.defaultProp),
+      resolvedProps,
+    ));
+    const { children: ignored, ...restResolvedProps } = wrappedProps;
+    if (loading && LoadingComponent !== DefaultLoadingComponent) {
+      return <LoadingComponent {...this.props} {...restResolvedProps} progress={progress} />;
     }
+    if (error) {
+      return <ErrorComponent {...this.props} {...restResolvedProps} progress={progress} error={error} />;
+    }
+    if (asyncComponent) {
+      if (this.state.component) {
+        Comp = this.state.component
+      } else {
+        return null;
+      }
+    }
+    if (Comp) {
+      return <Comp {...wrappedProps} progress={progress} />;
+    }
+    // noinspection JSUnresolvedFunction JSCheckFunctionSignatures
+    const newChildren = React.Children
+      .map(children, child => React.cloneElement(child, restResolvedProps));
+    if (newChildren.length === 1) {
+      return newChildren[0];
+    } else if (newChildren.length > 1) {
+      return majorVersion >= 16 ? newChildren : <div> {newChildren} </div>;
+    }
+    return null;
+  }
 }
 
 const {
@@ -202,6 +233,7 @@ AsyncComponent.propTypes = {
   })),
   asyncPropsMapper: func,
   component: func,
+  asyncComponent: func,
   children: oneOfType([element, arrayOf(element)]),
   errorComponent: func,
   loadingComponent: func,
@@ -216,6 +248,7 @@ AsyncComponent.defaultProps = {
   asyncPropOpts: {},
   asyncPropsMapper: props => props,
   component: null,
+  asyncComponent: null,
   children: null,
   errorComponent: DefaultErrorComponent,
   loadingComponent: DefaultLoadingComponent,
