810157f537fb665d94d51fccf7df02def3fad883
vipcxj <vipcxj@126.com>
2018-03-31 17:32:45 +0800

    make eslint happy

--- rollup.config.js
+++ rollup.config.js
@@ -6,53 +6,54 @@ import replace from 'rollup-plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
 import uglify from 'rollup-plugin-uglify';
 // import closure from 'rollup-plugin-closure-compiler-js';
-import camelCase from 'lodash/camelcase';
+import camelCase from 'lodash/camelCase';
 
-let file, sourcemap;
+let file;
+let sourcemap;
 if (process.env.NODE_ENV === 'production') {
-    file = `${process.env.BABEL_ENV}/${process.env.FILE_NAME}.min.${process.env.EXT}`;
-    sourcemap = false;
+  file = `${process.env.BABEL_ENV}/${process.env.FILE_NAME}.min.${process.env.EXT}`;
+  sourcemap = false;
 } else {
-    file = `${process.env.BABEL_ENV}/${process.env.FILE_NAME}.${process.env.EXT}`;
-    sourcemap = true;
+  file = `${process.env.BABEL_ENV}/${process.env.FILE_NAME}.${process.env.EXT}`;
+  sourcemap = true;
 }
 
 const config = {
-    input: 'src/index.js',
-    output: {
-        file,
-        name: `${camelCase(process.env.FILE_NAME)}`,
-        format: process.env.BABEL_ENV,
-        sourcemap,
-        globals: {
-            react: 'React',
-            'prop-types': 'PropTypes',
-        }
+  input: 'src/index.js',
+  output: {
+    file,
+    name: `${camelCase(process.env.FILE_NAME)}`,
+    format: process.env.BABEL_ENV,
+    sourcemap,
+    globals: {
+      react: 'React',
+      'prop-types': 'PropTypes',
     },
-    plugins: [
-        external(),
-        resolve({
-            jsnext: true,
-        }),
-        commonjs({
-            include: 'node_modules/**',
-        }),
-        babel({
-            exclude: "node_modules/**",
-        }),
-        replace({
-            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-        }),
-        // closure(),
-    ],
+  },
+  plugins: [
+    external(),
+    resolve({
+      jsnext: true,
+    }),
+    commonjs({
+      include: 'node_modules/**',
+    }),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+    // closure(),
+  ],
 };
 
 if (process.env.ANALYZER === true) {
-    config.plugins.push(analyzer());
+  config.plugins.push(analyzer());
 }
 
 if (process.env.NODE_ENV === 'production') {
-    config.plugins.push(uglify());
+  config.plugins.push(uglify());
 }
 
 // noinspection JSUnusedGlobalSymbols
